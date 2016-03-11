7957d1424e148d1196c840c016d9a53d21ffed38
cclauss <cclauss@bluewin.ch>
2016-03-11 21:22:14 +0100

    Update idapythonrc.py

--- idapythonrc.py
+++ idapythonrc.py
@@ -16,53 +16,49 @@ def start_trace():
     then execute this script in order to create dygraph trace.
     '''
     global codemap
-    if codemap.start is False and idc.GetProcessState() is idc.DSTATE_NOTASK:
-        print 'IDA debugger not running'
-        return
-
-    if codemap.start is True:
+    if codemap.start:
         codemap.pause = not codemap.pause
-        print 'Codemap Paused? : ', codemap.pause
-        if codemap.pause is False:    # resume tracing
-            continue_process()
-        else:
+        print('Codemap Paused? : ', codemap.pause)
+        if codemap.pause:
             codemap.db_insert()
             suspend_process()
+        else:  # resume tracing
+            continue_process()
+        return
+    elif idc.GetProcessState() is idc.DSTATE_NOTASK:
+        print('IDA debugger not running')
         return
 
     codemap.init_arch()
     codemap.skel = codemap.skel.replace('--ARCH--', codemap.arch.name)
     hook_ida()
 
-    print 'hook ida done.'
-    print 'homedir : ', codemap.homedir
-    print 'making table...'
+    print('hook ida done.')
+    print('homedir : ', codemap.homedir)
+    print('making table...')
     # initialize sqlite3 db
-    print codemap.db_create()
+    print(codemap.db_create())
 
     # set default SQL
     if codemap.arch.name is 'x86':
         codemap.query = "select eip from trace{0}".format(codemap.uid)
-    if codemap.arch.name is 'x64':
+    elif codemap.arch.name is 'x64':
         codemap.query = "select rip from trace{0}".format(codemap.uid)
 
     # if no baseaddr is configured then 0
-    if codemap.base == 0:
-        codemap.skel = codemap.skel.replace('--BASEADDR--', '0')
-    else:
-        codemap.skel = codemap.skel.replace(
-            '--BASEADDR--', hex(codemap.base).replace('0x', ''))
+    codemap.skel = codemap.skel.replace('--BASEADDR--',
+        (hex(codemap.base).replace('0x', '') if codemap.base else '0')
 
-    print 'start HTTP server'
+    print('start HTTP server')
     # start HTTP server
     codemap.start_webserver()
 
     # fire up chrome!
-    result = 'http://{0}:{1}/{2}'.format(codemap.server,
-                                         codemap.port,
-                                         codemap.uid)
+    result = 'http://{}:{}/{}'.format(codemap.server,
+                                      codemap.port,
+                                      codemap.uid)
     webbrowser.open(result)
-    print 'start tracing...'
+    print('start tracing...')
     codemap.start = True
     continue_process()
 
@@ -74,22 +70,21 @@ def set_function_bp():
     '''
     ea = ScreenEA()
     if ea == idaapi.BADADDR:
-        print "Could not get ScreenEA"
+        print("Could not get ScreenEA")
         return
 
     for chunk in Chunks(ea):
-        chunk_startEA = chunk[0]
-        chunk_endEA = chunk[1]
+        chunk_startEA, chunk_endEA = chunk[:2]
         target = 0
-        if chunk_startEA <= ea and ea <= chunk_endEA:
+        if chunk_startEA <= ea <= chunk_endEA:
             target = chunk_startEA
             break
 
     for fi in FuncItems(target):
-        if get_bpt(fi, bpt_t()) is False:
-            add_bpt(fi, 0, BPT_SOFT)
-        else:
+        if get_bpt(fi, bpt_t()):
             del_bpt(fi)
+        else:
+            add_bpt(fi, 0, BPT_SOFT)
 
 
 def set_range_bp():
@@ -98,20 +93,20 @@ def set_range_bp():
     '''
     ea = ScreenEA()
     if ea == idaapi.BADADDR:
-        print "Could not get ScreenEA"
+        print("Could not get ScreenEA")
         return
     start_addr = AskAddr(ea, 'Start Addr? (e.g. 0x8000) : ')
     end_addr = AskAddr(ea, 'End Addr? (e.g. 0xC000) : ')
 
-    if start_addr is None or end_addr is None:
-        print "AskAddr error"
+    if not start_addr or not end_addr:
+        print("AskAddr error")
         return
 
     for e in Heads(start_addr, end_addr):
-        if get_bpt(e, bpt_t()) is False:
-            add_bpt(e, 0, BPT_SOFT)
-        else:
+        if get_bpt(e, bpt_t()):
             del_bpt(e)
+        else:
+            add_bpt(e, 0, BPT_SOFT)
 
 
 def save_module_bp():
@@ -126,14 +121,13 @@ def save_module_bp():
             bpo = ''
             for func in Functions():
                 for chunk in Chunks(func):
-                    chunk_startEA = chunk[0]
-                    chunk_endEA = chunk[1]
+                    chunk_startEA, chunk_endEA = chunk[:2]
                     length = chunk_endEA - chunk_startEA
                     if length < codemap.func_min_size:
                         continue
                     offset = chunk_startEA - get_imagebase()
                     bpo += str(offset) + '\n'
-            print 'bp offset generation complete! ' + str(len(bpo))
+            print('bp offset generation complete! ' + str(len(bpo)))
             payload = bpo
             with open(codemap.homedir + modname + '.bpo', 'wb') as f:
                 f.write(zlib.compress(payload))
@@ -167,7 +161,7 @@ def load_module_bp():
             code = bytearray()
             for bp in bps:
                 code += 'add_bpt({0}, 0, BPT_SOFT);'.format(baseaddr + int(bp))
-            print 'setting breakpoints...'
+            print('setting breakpoints...')
             # set bp!
             exec(str(code))
     except:
@@ -176,7 +170,7 @@ def load_module_bp():
 
 def set_module_bp():
     global codemap
-    if codemap.start is False:
+    if not codemap.start:
         if idc.GetProcessState() is idc.DSTATE_NOTASK:
             save_module_bp()
         else:
@@ -186,7 +180,7 @@ def set_module_bp():
 def listen_codemap():
     global codemap
     codemap.start_websocketserver()
-    print "Listning to codemap connection..."
+    print("Listning to codemap connection...")
 
 
 # print slows IDA down
@@ -199,13 +193,13 @@ class IDAHook(DBG_Hooks):
               (pid, tid, ea, code))
 
     def dbg_bpt(self, tid, ea):
-        if codemap.pause is True:
-            return 0    # stop visualizing
+        if codemap.pause:
+            return 0  # stop visualizing
 
         codemap.set_data()
         codemap.db_insert_queue()
-        continue_process()                  # continue
-        return 0    # no warning
+        continue_process()  # continue
+        return 0  # no warning
 
 
 def hook_ida():
@@ -237,9 +231,9 @@ AddHotkey('Alt-3', 'key_3')
 AddHotkey('Alt-4', 'key_4')
 AddHotkey('Alt-5', 'key_5')
 
-print 'ALT-1 : Start/Stop Codemap'
-print 'ALT-2 : Set Function BP'
-print 'ALT-3 : Set Range BP'
-print 'ALT-4 : Create/Setup Module BP'
-print 'ALT-5 : Connect Codemap Graph with IDA'
-print 'Codemap Python Plugin is ready. enjoy. - by daehee'
+print('''ALT-1 : Start/Stop Codemap
+ALT-2 : Set Function BP
+ALT-3 : Set Range BP
+ALT-4 : Create/Setup Module BP
+ALT-5 : Connect Codemap Graph with IDA
+Codemap Python Plugin is ready. enjoy. - by daehee''')
